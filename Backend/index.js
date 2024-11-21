6c5d7cbe25fe21d0266c1e50e43a202a1adf86db
adityabhawsar21 <adityabhawsar21@gmail.com>
2024-11-21 14:28:30 +0530

    app completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,20 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
+
+// code for deployment
+if(process.env.NODE_ENV === "production"){
+    const dirPath = path.resolve();
+
+    app.use(express.static("./Frontend/dist"));
+    app.get("*" , (req,res) => {
+        res.sendFile(path.resolve(dirPath ,"./Frontend/dist" , "index.html"));
+    })
+}
 
 dotenv.config();
 
